25082af9dc6c3ac3a26038666f879226b1c80e9f
futaro <machidada@gmail.com>
2012-10-12 00:18:01 +0900

    bash-completionのかいちゃまずいよね

--- .bash
+++ .bash
@@ -1,9 +1,4 @@
 
-# for bash_completion
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-  source `brew --prefix`/etc/bash_completion
-fi
-
 export CLICOLOR=1
 export LSCOLORS=DxGxcxdxCxegedabagacad
 
