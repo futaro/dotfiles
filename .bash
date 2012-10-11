71a20c17645098670b29c2cdeb9f87a1960f0c0c
futaro <machidada@gmail.com>
2012-10-12 00:16:23 +0900

    zenburn

--- .bash
+++ .bash
@@ -1,4 +1,9 @@
 
+# for bash_completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  source `brew --prefix`/etc/bash_completion
+fi
+
 export CLICOLOR=1
 export LSCOLORS=DxGxcxdxCxegedabagacad
 
