8677cfda6e379bec4638b3f5fde5bb001c4f9666
Masashi Machida <masashi.machida@aiasahi.jp>
2017-11-08 11:30:56 +0900

    hoge

--- .bash
+++ .bash
@@ -5,6 +5,6 @@ export CLICOLOR=1
 export IGNOREEOF=3
 
 alias ls='ls -F'
-alias ll='ls -l'
+alias ll='ls -la'
 alias dsstore='find . -name ".DS_Store" | xargs rm'
 alias dockerrm='docker rm `docker ps -a -q` && docker rmi $(docker images | awk "/^<none>/ { print $3 }")'
